663bbcd7091de8c37e947142df491e9e7e4c78fa
Johnny Chadda <johnny@chadda.se>
2014-02-25 17:27:28 +0100

    Fixed janus detection

--- apps.d/janus/install.sh
+++ apps.d/janus/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ! -d "$HOME/.janus" ]; then
+if [ ! -d "$HOME/.vim/janus" ]; then
     curl -Lo- https://bit.ly/janus-bootstrap | bash
     success "$app installed"
 else
