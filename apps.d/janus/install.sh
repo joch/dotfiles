810210cb13d9e9e6e3228dd3bf40dc88aeb81789
Johnny Chadda <johnny@chadda.se>
2015-02-16 21:21:45 +0100

    Install vim-gitgutter

--- apps.d/janus/install.sh
+++ apps.d/janus/install.sh
@@ -4,6 +4,12 @@ set -e
 if [ ! -d "$HOME/.vim/janus" ]; then
     curl -Lo- https://bit.ly/janus-bootstrap | bash
     success "$app installed"
+
+    info "Installing vim-gitgutter"
+    mkdir -p ~/.janus
+    git clone git://github.com/airblade/vim-gitgutter.git ~/.janus/vim-gitgutter
+    success "installed"
+
 else
     warning "$app already installed, skipping..."
 fi
